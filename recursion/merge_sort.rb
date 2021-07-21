a30999ab593bee6bbdaa8e54dd46ec0af0517e0a
ong-wei-hong <ongweihong@protonmail.com>
2021-07-21 14:01:54 +0800

    polished recursion/merge_sort.rb

--- recursion/merge_sort.rb
+++ recursion/merge_sort.rb
@@ -2,22 +2,19 @@
 
 def merge(arr1, arr2)
   arr = []
-  while arr1.length > 0 || arr2.length > 0
+  loop do
     if arr1.length == 0
-      arr += arr2
-      arr2 = []
+      return arr += arr2
     elsif arr2.length == 0
-      arr += arr1
-      arr1 = []
+      return arr += arr1
     else
       arr << (arr1.first > arr2.first ? arr2.shift : arr1.shift)
     end
   end
-  arr
 end
 
 def merge_sort(arr)
-  if arr.length == 1
+  if arr.length < 2
     return arr
   end
   merge(merge_sort(arr[0..arr.length / 2 - 1]), merge_sort(arr[arr.length / 2..arr.length - 1]))
